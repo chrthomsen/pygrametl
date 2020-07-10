688dd9369130afc2b7e9244b91bbdda8e54e6e47
Soeren Kejser Jensen <devel@kejserjensen.dk>
2020-07-10 11:11:39 +0200

    Update copyright year in LICENSE and documentation

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pygrametl'
-copyright = u'2009 - 2016, Aalborg University'
+copyright = u'2009 - 2020, Aalborg University'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
