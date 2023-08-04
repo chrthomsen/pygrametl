16ce206f86ad95a450e4fdcb532986e351802f4d
Christian Thomsen <chr@cs.aau.dk>
2023-08-04 17:56:10 +0200

    some initial tests

---
+++ tests/tables/test_SlowlyChangingDimension_lookupasof.py
@@ -0,0 +1,82 @@
+import unittest
+
+from tests import utilities
+import pygrametl
+import pygrametl.drawntabletesting as dtt
+from pygrametl.tables import SlowlyChangingDimension
+
+
+class SlowlyChangingDimensionLookupasofTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        utilities.ensure_default_connection_wrapper()
+        cls.initial = dtt.Table("customers", """
+        | id:int (pk) | name:varchar | city:varchar | fromdate:timestamp | todate:timestamp | version:int |
+        | ----------- | ------------ | ------------ | ------------------ | ---------------- | ----------- |
+        | 1           | Ann          | Aalborg      | 2001-01-01         | 2001-12-31       | 1           |
+        | 2           | Bob          | Boston       | 2001-01-01         | 2001-12-31       | 1           |
+        | 3           | Ann          | Aarhus       | 2002-01-01         | 2002-12-31       | 2           |
+        | 4           | Charlie      | Copenhagen   | 2001-01-01         | NULL             | 1           |
+        | 5           | Ann          | Aabenraa     | 2003-01-01         | NULL             | 3           |
+        | 6           | Bob          | Birkelse     | 2002-01-01         | 2002-12-31       | 2           |
+        """)
+
+    def setUp(self):
+        utilities.ensure_default_connection_wrapper()
+        self.initial.reset()
+        self.connection_wrapper = pygrametl.getdefaulttargetconnection()
+        self.ann = {'name':'Ann'}
+        self.bob = {'name':'Bob'}
+
+
+    def test_lookupasof_usingto(self):
+        test_dimension = SlowlyChangingDimension(
+            name=self.initial.name,
+            key=self.initial.key(),
+            attributes=self.initial.attributes,
+            lookupatts=['name'],
+            versionatt='version',
+            toatt='todate',
+            cachesize=100,
+            prefill=False)
+        key = test_dimension.lookupasof(self.ann, "2001-05-05", True)
+        self.assertEqual(key, 1)
+        key = test_dimension.lookupasof(self.ann, "2001-12-31", True)
+        self.assertEqual(key, 1)
+        key = test_dimension.lookupasof(self.ann, "2001-12-31", False)
+        self.assertEqual(key, 3) # intended behaviour since we ignore fromdate in this test
+        key = test_dimension.lookupasof(self.ann, "2222-12-31", True)
+        self.assertEqual(key, 5)
+        key = test_dimension.lookupasof(self.bob, "2222-12-31", True)
+        self.assertEqual(key, None)
+
+    
+
+    def test_lookupasof_usingfrom(self):
+        test_dimension = SlowlyChangingDimension(
+            name=self.initial.name,
+            key=self.initial.key(),
+            attributes=self.initial.attributes,
+            lookupatts=['name'],
+            versionatt='version',
+            fromatt='fromdate',
+            cachesize=100,
+            prefill=False)
+        # TODO
+
+
+    def test_lookupasof_usingfromto(self):
+        test_dimension = SlowlyChangingDimension(
+            name=self.initial.name,
+            key=self.initial.key(),
+            attributes=self.initial.attributes,
+            lookupatts=['name'],
+            versionatt='version',
+            fromatt='fromdate',
+            toatt='todate',
+            cachesize=100,
+            prefill=False)
+        # TODO
+
+
